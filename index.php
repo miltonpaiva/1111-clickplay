657985d187aa9480957a0717b1246156daa38811
miltonpaiva <miltonpaiva268@gmail.com>
2020-07-12 11:31:09 -0300

    adição do iniciador do click pkay logo abaixo da galeria

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php get_header(); ?>
 <main>
 
-    <!--[foogallery id="502"]-->
-    <?php echo do_shortcode('[foogallery id="502"]'); ?>
+    <!--[foogallery id="5"]-->
+    <?php echo do_shortcode('[foogallery id="5"]'); ?>
         
 </main>
+<script>
+	// INICIADOR DA FUNÇÃO DO CLICK PLAY
+	clickPlay('fg-thumb');	
+</script>
 <?php get_footer(); ?>
